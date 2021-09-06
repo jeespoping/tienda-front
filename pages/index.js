1ec00777f757d19f922102e5764dbf910036fc73
jeespoping <jeespoping@gmail.com>
2021-09-06 09:45:19 -0500

    importando semantic

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from "next/head";
+import { Button } from "semantic-ui-react";
 
 export default function Home() {
   return (
     <div className="home">
       <h1>Estamos en N3xt Js</h1>
+      <Button primary>Primary</Button>
+      <Button secondary>Secondary</Button>
     </div>
   );
 }
