ad7bbaa28f80d6aea5e02e70c69c409f6891cb0c
jeespoping <jeespoping@gmail.com>
2021-09-24 08:36:43 -0500

    formik yup login

--- components/Auth/LoginForm/LoginForm.js
+++ components/Auth/LoginForm/LoginForm.js
@@ -1,20 +1,34 @@
 import { useState } from "react";
 import { Form, Button } from "semantic-ui-react";
-import { useFormik } from "formik";
+import { Formik, useFormik } from "formik";
 import * as Yup from "yup";
 import { toast } from "react-toastify";
 
 export default function LoginForm({ showRegisterForm }) {
-  const [state, setstate] = useState(false);
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: (formData) => {
+      console.log(formData);
+    },
+  });
 
   return (
-    <Form className="login-form">
+    <Form className="login-form" onSubmit={formik.handleSubmit}>
       <Form.Input
         name="identifier"
         type="text"
         placeholder="Correo electronico"
+        onChange={formik.handleChange}
+        error={formik.errors.identifier}
       />
-      <Form.Input name="password" type="password" placeholder="Contraseña" />
+      <Form.Input
+        name="password"
+        type="password"
+        placeholder="Contraseña"
+        onChange={formik.handleChange}
+        error={formik.errors.password}
+      />
       <div className="actions">
         <Button type="button" basic onClick={showRegisterForm}>
           Registrarse
@@ -28,4 +42,18 @@ export default function LoginForm({ showRegisterForm }) {
       </div>
     </Form>
   );
+}
+
+function initialValues() {
+  return {
+    identifier: "",
+    password: "",
+  };
+}
+
+function validationSchema() {
+  return {
+    identifier: Yup.string().email(true).required(true),
+    password: Yup.string().required(true),
+  };
 }
