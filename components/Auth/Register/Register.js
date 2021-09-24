f97645f516620cdbb65b2015e9e2df577ec08b51
jeespoping <jeespoping@gmail.com>
2021-09-24 07:32:02 -0500

    formulario

--- components/Auth/Register/Register.js
+++ components/Auth/Register/Register.js
@@ -1,7 +1,20 @@
+import { Form, Button } from "semantic-ui-react";
+
 export default function Register({ showLoginForm }) {
   return (
-    <div>
-      <button onClick={showLoginForm}>Ir a login</button>
-    </div>
+    <Form className="login-form">
+      <Form.Input name="name" type="text" placeholder="Nombre" />
+      <Form.Input name="lastname" type="text" placeholder="Apellidos" />
+      <Form.Input name="username" type="text" placeholder="Nombre de usuario" />
+      <Form.Input name="email" type="text" placeholder="Correo electronico" />
+      <Form.Input name="password" type="password" placeholder="Contraseña" />
+
+      <div className="actions">
+        <Button type="button">Iniciar sesion</Button>
+        <Button className="submit" type="submit">
+          Registrar
+        </Button>
+      </div>
+    </Form>
   );
 }
