a3413df3608397aaec3fc0febf924a0d85154b5e
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-06-05 10:38:25 -0500

    Majeando los datos del formulario de email

--- components/Account/ChangeEmailForm/ChangeEmailForm.js
+++ components/Account/ChangeEmailForm/ChangeEmailForm.js
@@ -1,17 +1,37 @@
 import React from "react";
 import { Form, Button } from "semantic-ui-react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { toast } from "react-toastify";
 
 export default function ChangeEmailForm({ user, logout, setReloadUser }) {
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: (formData) => {
+      console.log(formData);
+    },
+  });
+
   return (
     <div className="change-email-form">
       <h4>
         Cambia tu e-mail <span>(Tu e-email acutal: {user.email} </span>
       </h4>
 
-      <Form>
+      <Form onSubmit={formik.handleSubmit}>
         <Form.Group widths="equal">
-          <Form.Input name="email" placeholder="Tu numero e-mail" />
           <Form.Input
+            value={formik.values.email}
+            error={formik.errors.email}
+            onChange={formik.handleChange}
+            name="email"
+            placeholder="Tu nuevo e-mail"
+          />
+          <Form.Input
+            value={formik.values.repeatEmail}
+            error={formik.errors.repeatEmail}
+            onChange={formik.handleChange}
             name="repeatEmail"
             placeholder="Confirma tu nuevo e-mail"
           />
@@ -20,4 +40,24 @@ export default function ChangeEmailForm({ user, logout, setReloadUser }) {
       </Form>
     </div>
   );
+}
+
+function initialValues() {
+  return {
+    email: "",
+    repeatEmail: "",
+  };
+}
+
+function validationSchema() {
+  return {
+    email: Yup.string()
+      .email(true)
+      .required(true)
+      .oneOf([Yup.ref("repeatEmail")], true),
+    repeatEmail: Yup.string()
+      .email(true)
+      .required(true)
+      .oneOf([Yup.ref("email")], true),
+  };
 }
