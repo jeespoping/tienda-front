45d04ea8848e567ca388322572bc3fe64faed2a7
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-06-05 16:34:57 -0500

    Obteniendo juegos por plataforas

--- pages/games/[platform].js
+++ pages/games/[platform].js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import BasicLayagout from "../../layouts/BasicLayout";
 import { useRouter } from "next/router";
+import { getGamesPlatformApi } from "../../api/game";
+
+const limitPerPage = 10;
 
 export default function Platform() {
   const { query } = useRouter();
+  const [games, setGames] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const response = await getGamesPlatformApi(
+        query.platform,
+        limitPerPage,
+        0
+      );
+      setGames(response);
+    })();
+  }, [query]);
+
   return (
     <BasicLayagout className="platform">
       <h1>Estamos en la plataforma : {query.platform}</h1>
