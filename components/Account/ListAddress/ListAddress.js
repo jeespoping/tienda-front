a3b4163d3067471fd577dfd345520d02aafe2530
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-06-05 12:47:10 -0500

    Listando direcciones

--- components/Account/ListAddress/ListAddress.js
+++ components/Account/ListAddress/ListAddress.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { Grid, Button } from "semantic-ui-react";
+import { map, size } from "lodash";
 import { getAddressesApi } from "../../../api/address";
 import useAuth from "../../../hooks/useAuth";
 
@@ -12,5 +14,38 @@ export default function ListAddress() {
     })();
   }, []);
 
-  return <div>ListAddress</div>;
+  return (
+    <div className="list-address">
+      {size(addresses) === 0 ? (
+        <h3>No hay ninguna direccón creada</h3>
+      ) : (
+        <Grid>
+          {map(addresses, (address) => (
+            <Grid.Column key={address.id} mobile={16} tablet={8} computer={4}>
+              <Address address={address} />
+            </Grid.Column>
+          ))}
+        </Grid>
+      )}
+    </div>
+  );
+}
+
+function Address({ address }) {
+  return (
+    <div className="address">
+      <p>{address.title}</p>
+      <p>{address.name}</p>
+      <p>{address.address}</p>
+      <p>
+        {address.state}, {address.city} {address.postalCode}
+      </p>
+      <p>{address.phone}</p>
+
+      <div className="actions">
+        <Button primary>Editar</Button>
+        <Button>Eliminar</Button>
+      </div>
+    </div>
+  );
 }
