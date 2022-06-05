b6908bda93c9d4c46dd710f87b7981d746d6484e
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-06-05 16:15:47 -0500

    Estructura y estilo de la lista de juegos

--- components/ListGames/ListGames.js
+++ components/ListGames/ListGames.js
@@ -1,12 +1,41 @@
 import React from "react";
+import { Image, Grid } from "semantic-ui-react";
+import Link from "next/link";
 import { map } from "lodash";
 
 export default function ListGames({ games }) {
   return (
     <div className="list-games">
-      {map(games, (game) => (
-        <h3>{game.title}</h3>
-      ))}
+      <Grid>
+        <Grid.Row columns={5}>
+          {map(games, (game) => (
+            <Game game={game} />
+          ))}
+        </Grid.Row>
+      </Grid>
     </div>
+  );
+}
+
+function Game({ game }) {
+  return (
+    <Grid.Column className="list-games__game">
+      <Link href={`/${game.url}`}>
+        <a>
+          <div className="list-games__game-poster">
+            <Image src={game.poster.url} alt={game.title} />
+            <div className="list-games__game-poster-info">
+              {game.discount ? (
+                <span className="discount">-{game.discount}%</span>
+              ) : (
+                <span />
+              )}
+              <span className="price">{game.price}USD</span>
+            </div>
+          </div>
+          <h2>{game.title}</h2>
+        </a>
+      </Link>
+    </Grid.Column>
   );
 }
