453b9459b51514fe7d17b07ba704531d17ce7684
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-06-05 15:49:38 -0500

    Obteniendo los ultimos juegos añadidos

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,20 @@
+import React, { useState, useEffect } from "react";
+import { size } from "lodash";
+import { getLastGamesApi } from "../api/game";
 import BasicLayout from "../layouts/BasicLayout";
 
 export default function Home() {
+  const [games, setGames] = useState(null);
+  console.log(games);
+
+  useEffect(() => {
+    (async () => {
+      const response = await getLastGamesApi(50);
+      if (size(response) > 0) setGames(response);
+      else setGames([]);
+    })();
+  }, []);
+
   return (
     <BasicLayout className="home">
       <h1>Estamos en home</h1>
