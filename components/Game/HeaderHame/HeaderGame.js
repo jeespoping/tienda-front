ea5bd6f90e2a4cf1bb1cca74f95b98339ed62008
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-06-05 20:35:12 -0500

    Añadiendo la informacion del juego

--- components/Game/HeaderHame/HeaderGame.js
+++ components/Game/HeaderHame/HeaderGame.js
@@ -11,8 +11,35 @@ export default function HeaderGame({ game }) {
         <Image src={poster.url} alt={title} fluid />
       </Grid.Column>
       <Grid.Column mobile={16} tablet={10} computer={11}>
-        <p>info Game</p>
+        <Info game={game} />
       </Grid.Column>
     </Grid>
+  );
+}
+
+function Info({ game }) {
+  const { title, summary, price, discount } = game;
+  return (
+    <>
+      <div className="header-game__title">
+        {title}
+        <Icon name="heart outline" link />
+      </div>
+      <div className="header-game__delivery">Entrega en 24/48h</div>
+      <div
+        className="header-game__summary"
+        dangerouslySetInnerHTML={{ __html: summary }}
+      />
+      <div className="header-game__buy">
+        <div className="header-game__buy-price">
+          <p>Precio de venta al publico: {price}USD</p>
+          <div className="header-game__buy-price-actions">
+            <p>-{discount}%</p>
+            <p>{price - Math.floor(price * discount) / 100}USD</p>
+          </div>
+        </div>
+        <Button className="header-game__buy-btn">Comprar</Button>
+      </div>
+    </>
   );
 }
