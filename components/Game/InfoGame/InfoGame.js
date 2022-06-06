cdd111dec99586d70ce1e1e704a80abeae57bb48
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-06-06 12:58:15 -0500

    Fecha de lanzamiento y descripcion

--- components/Game/InfoGame/InfoGame.js
+++ components/Game/InfoGame/InfoGame.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactPlayer from "react-player/lazy";
+import moment from "moment";
+import "moment/locale/es";
 import CarrouselScreenshots from "../CarrouselScreenshots/CarrouselScreenshots";
 
 export default function InfoGame({ game }) {
@@ -11,6 +13,13 @@ export default function InfoGame({ game }) {
         url={game.video}
       />
       <CarrouselScreenshots title={game.title} screenshots={game.screenshots} />
+      <div className="info-game__content">
+        <div dangerouslySetInnerHTML={{ __html: game.summary }} />
+        <div className="info-game__content-date">
+          <h4>Fecha de lanzamiento:</h4>
+          <p>{moment(game.releaseDate).format("LL")}</p>
+        </div>
+      </div>
     </div>
   );
 }
