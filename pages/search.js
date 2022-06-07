70fedabf3edab7e64d8e4b4730dfbc9717fbd3df
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-06-06 20:49:13 -0500

    Creando la pagina de busqueda

---
+++ pages/search.js
@@ -0,0 +1,6 @@
+import React from "react";
+import BasicLayout from "../layouts/BasicLayout";
+
+export default function search() {
+  return <BasicLayout className={search}>search</BasicLayout>;
+}
