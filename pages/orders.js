00e545ff165557b41faf41c15ea0028bc54053bf
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-06-08 18:59:35 -0500

    Onteniendo todos los pedidos del usuario

--- pages/orders.js
+++ pages/orders.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Grid } from "semantic-ui-react";
 import BasicLayout from "../layouts/BasicLayout";
+import useAuth from "../hooks/useAuth";
+import { getOrdersApi } from "../api/order";
 
 export default function orders() {
+  const [orders, setOrders] = useState(null);
+  const { auth, logout } = useAuth();
+
+  useEffect(() => {
+    (async () => {
+      const response = await getOrdersApi(auth.idUser, logout);
+      setOrders(response || []);
+    })();
+  }, []);
+
   return (
     <BasicLayout className="orders">
       <div className="orders__block">
