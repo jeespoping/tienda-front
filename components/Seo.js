2e5b453902892d17c7fa23fe2e983a66af698bbb
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2022-06-08 20:08:43 -0500

    Creando el componente SEO

---
+++ components/Seo.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Head from "next/head";
+
+export default function Seo({ title, description }) {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta property="description" content="description" />
+    </Head>
+  );
+}
+
+Seo.defaultProps = {
+  title: "Gaming - Tus juegos favoritos",
+  description: "Tus juegos favoritos para Steam, PlayStation, XBOX",
+};
