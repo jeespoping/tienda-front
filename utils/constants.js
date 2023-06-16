ea07dfa158ccb4975bea374ce4c12edc5d0cb013
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2023-06-16 18:19:56 -0500

    Agregado el buscador

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
-export const BASE_PATH = "https://eccomerce-game.herokuapp.com";
+export const BASE_PATH = "http://localhost:1337";
 export const TOKEN = "token";
 export const CART = "cart";
 export const STRIPE_TOKEN = "pk_test_a3LrtAYwxYPQevNs28FYyoRp00e9rV7npx";
